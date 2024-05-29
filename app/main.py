f59c0a2f84a5384865dc3ae658f51bfab1b052b6
Juan Moreno <jdmorenov22@gmail.com>
2024-05-28 21:15:55 -0500

    version 1.2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
 from app.model import predict_result
 from app.schema import InputData, PredictionResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+# Lista de orígenes permitidos
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://tu-dominio.com",
+    # Agrega aquí otros dominios permitidos
+]
+
+# Agregar el middleware de CORS a la aplicación
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir estos orígenes
+    allow_credentials=True,  # Permitir el envío de cookies
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
+
+
 
 @app.post("/predict", response_model=PredictionResponse)
 async def predict(data: InputData):
