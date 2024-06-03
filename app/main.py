df271a1544dc50ec13c2afba3642320c0ad62cb6
Juan Moreno <jdmorenov22@gmail.com>
2024-06-03 14:58:56 -0500

    versión 1.4

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://chess-prediction.vercel.app/"
+    "https://chess-prediction.vercel.app"
     # Agrega aquí otros dominios permitidos
 ]
 
